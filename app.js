812607f7891ea51e6c1c20fc9ac68b8717f5e00f
samuel boluwatife <you@example.com>
2025-08-29 13:07:28 +0100

    updated code

--- app.js
+++ app.js
@@ -6,8 +6,41 @@ const form = document.querySelector(".get-started-form");
     navMenu.classList.toggle('show');
       });
 
+  const points = [
+  " Corrective joint rehabilitation for older adults and those in recovery",
+  " Functional training for limitless strength endurance",
+  " Body recomposition and nutrition strategies for lasting health"
+];
 
- 
+let index = 0;
+let charIndex = 0;
+const typewriterEl = document.getElementById("typewriter");
+
+function typeEffect() {
+  if (charIndex < points[index].length) {
+    typewriterEl.textContent += points[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 20); // typing speed
+  } else {
+    setTimeout(eraseEffect, 1500); // wait before erasing
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    typewriterEl.textContent = points[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 20); // erasing speed
+  } else {
+    index = (index + 1) % points.length;
+    setTimeout(typeEffect, 200); // start typing next
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeEffect();
+});
+
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
